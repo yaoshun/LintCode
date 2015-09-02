8c2fdc6971a243357bfa189ac1471a9e081a4cfa
Yao <syao@hpc1.csc.bnl.gov>
2015-09-02 13:47:56 -0400

    assignment operator overloading

---
+++ assignment-operator-overloading-c++-only.cpp
@@ -0,0 +1,36 @@
+// LintCode operator overloading example
+
+class Solution {
+public:
+    char *m_pData;
+    Solution() {
+        this->m_pData = NULL;
+    }
+    Solution(char *pData) {
+        this->m_pData = pData;
+    }
+
+    // Implement an assignment operator
+    Solution operator=(const Solution &object) {
+        // write your code here
+        // Check whether they are the same object.
+        // this is a pointer.
+        if (this != &object) {
+            char *new_pData = NULL;
+            if (object.m_pData != NULL) {
+                size_t cnt = strlen(object.m_pData);
+                new_pData = new char[cnt];
+                copy(object.m_pData, object.m_pData + cnt, new_pData);
+            }
+            
+            if (m_pData != NULL)
+                delete m_pData;
+            
+            m_pData = new_pData;
+        }
+        
+        // Return the Solution object.
+        return *this;
+    }
+};
+
