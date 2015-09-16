30f7f2dce63d163f10df80345d23ff47bf5bd8b6
Yao <syao@hpc1.csc.bnl.gov>
2015-09-16 01:53:45 -0400

    jump game

--- jump-game.cpp
+++ jump-game.cpp
@@ -27,3 +27,35 @@ public:
         return lowest == 0;
     }
 };
+
+// Forward propagation.
+class Solution {
+public:
+    /**
+     * @param A: A list of integers
+     * @return: The boolean answer
+     */
+    bool canJump(vector<int> A) {
+        // write you code here
+        if (A.size() <= 1)
+            return true;
+
+        int n = A.size();
+
+        // lowest records the lowest index that can be backtracked from n - 1;
+        // DP would check whether any node that the current cover range is already set
+        // true, however we realized that only need to keep track of the lowest.
+        int largest = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i > largest) {
+                return false;
+            } else if (A[i] + i >= largest) {
+                largest = A[i] + i;
+            }
+        }
+
+        return true;
+    }
+};
+
